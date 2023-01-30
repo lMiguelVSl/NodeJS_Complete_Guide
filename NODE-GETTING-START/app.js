0f0cc46b5515b65cf01cd2e18e6d637c2fffa04c
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-01-29 20:02:36 -0500

    Sending request respose

--- NODE-GETTING-START/app.js
+++ NODE-GETTING-START/app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('req', req);
+    console.log('req url', req.url);
+    console.log('req method', req.method);
+    console.log('req headers', req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><header><title>My first node page</title></header></html>');
+    res.write('<body><h1>Hello from my nodeJS server!</h1></body>');
+    res.end();
     // how to hard end the server => process.exit();
 });
 
