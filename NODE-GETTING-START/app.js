8ca991da890cae8f6642291c20e474aede6a3341
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-01 21:43:02 -0500

    Code segmentation

--- NODE-GETTING-START/app.js
+++ NODE-GETTING-START/app.js
@@ -1,44 +1,6 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const routes = require('./routes');
 
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<header><title>My first node page</title></header>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-
-        req.on('data', (chunk) => {
-            console.log('chunk', chunk);
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            console.log('parse body', parseBody);
-            fs.writeFileSync('Txt Files/message.txt', message.replace('+\g', ' '));
-        });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><header><title>My first node page</title></header></html>');
-    res.write('<body><h1>Hello from my nodeJS server!</h1></body>');
-    res.end();
-    // how to hard end the server => process.exit();
-});
-
+const server = http.createServer(routes);
 server.listen(3000);
