b935cd79b5e1ad253414f741686da0744bc49e73
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-04 13:22:31 -0500

    Creating the html structure pages

--- NODE-EXPRESS/routes/admin.js
+++ NODE-EXPRESS/routes/admin.js
@@ -2,18 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/add-product', (req, res, next) => {
+// /admin route
+router.get('/add-product', (req, res, next) => {
     res.send(
         `<h1>add-product express middleware</h1>
          <hr>
-         <form action="/product" method="POST" >
+         <form action="/admin/add-product" method="POST" >
             <input type="text" name="Product">
             <button type="submit">Send</button>
          </form>`
     );
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 })
