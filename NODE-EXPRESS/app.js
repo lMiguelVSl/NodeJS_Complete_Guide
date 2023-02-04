996b9fd794ad35a76fd0426471138cc9588451db
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-04 09:46:41 -0500

    Using middlewares and finishing the response

--- NODE-EXPRESS/app.js
+++ NODE-EXPRESS/app.js
@@ -2,6 +2,15 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
+app.use((req, res, next) => {
+    console.log('MIDDLEWARE!');
+    next(); //continue with the next one
+});
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('ANOTHER MIDDLEWARE!');
+    res.write('<h1>Second express middleware</h1>');
+    res.end();
+});
+
+app.listen(3000);
