7e427bcd4fd2107c55f297c6749adaaec03596d0
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-04 09:13:18 -0500

    Managnig in different files the server and logic

--- NODE-ASSIGNMENT/app.js
+++ NODE-ASSIGNMENT/app.js
@@ -1,42 +1,6 @@
 const http = require('http');
-const fs = require('fs');
 
-const routes = require('./routes');
+const routes = require('./routes.js');
 
-const server = http.createServer((req, res) => {
-
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body></html>');
-        return res.end();
-    }
-    if (url === '/users') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><body><ul><li>User 1</li><li>User 2</li></ul></body></html>');
-        return res.end();
-    }
-    if (url === '/create-user' && method === "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1].split('+').join(' ');
-            fs.writeFileSync('Txt Files/message.txt', message);
-        });
-
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-
-
-    res.write('<html><body>404 Not Found</body></html>');
-
-});
+const server = http.createServer(routes.requestHandler);
 server.listen(3000);
