4911440f89f595ed29979610ccc1834fcb3e0a64
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-05 15:56:12 -0500

    global path

--- NODE-EXPRESS/routes/admin.js
+++ NODE-EXPRESS/routes/admin.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../helpers/path');
 
 const router = express.Router();
 
 // /admin route
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir.path, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
--- NODE-EXPRESS/routes/shop.js
+++ NODE-EXPRESS/routes/shop.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 
+const rootDir = require('../helpers/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir.path, 'views', 'shop.html'));
 });
 
 module.exports = router;
