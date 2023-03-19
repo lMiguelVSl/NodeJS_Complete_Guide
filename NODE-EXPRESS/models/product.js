d0bb9569c88c77f3090bea04c50eab23d9bee588
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-03-19 14:44:54 -0500

    saving data inf json file and callbacks implementation

--- NODE-EXPRESS/models/product.js
+++ NODE-EXPRESS/models/product.js
@@ -1,4 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
 const products = [];
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) return scb([]);
+        cb(JSON.parse(fileContent));
+    });
+};
 
 module.exports = class Product {
     constructor(_title) {
@@ -6,10 +17,13 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => console.log(err));
+        });
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        getProductsFromFile(cb);
     }
 }
