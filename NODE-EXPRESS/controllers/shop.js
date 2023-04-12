d8426a4e220db68b84e43f8c1430b050a63f13b8
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-04-11 22:24:42 -0500

    details product navigation and params

--- NODE-EXPRESS/controllers/shop.js
+++ NODE-EXPRESS/controllers/shop.js
@@ -10,6 +10,11 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  console.log('produt id from params url', prodId);
+}
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
